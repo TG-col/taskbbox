77b8448aee95d0493a2e766d69d548cd07d30fff
tsipi.gibor <tsipi.gibor@colmobil.co.il>
2021-12-07 17:11:18 +0200

    SimpleComponent2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDir: ["../public"],
   addons: [
     "@storybook/addon-links",
@@ -8,3 +8,14 @@ module.exports = {
     "@storybook/addon-interactions",
   ],
 };
+//stories: ["../src/components/**/*.stories.js"],
+// module.exports = {
+//   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+//   staticDir: ["../public"],
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/preset-create-react-app",
+//     "@storybook/addon-interactions",
+//   ],
+// };
